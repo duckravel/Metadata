ac593037fed3cb538d208376af43a388db67abf6
duckravel <al396421@uji.es>
2020-11-04 18:12:03 +0100

    intro page

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
 <div>   
-  <nav class="navbar navbar-expand-lg border-bottom d-flex justify-content-center" style='background-color: rgba(255, 255, 255, 0.863);' id='menu'>
+  <!-- <nav class="navbar navbar-expand-lg border-bottom d-flex justify-content-center" style='background-color: rgba(255, 255, 255, 0.863);' id='menu'>
         <div class='h2'>Speech Based Metadata Creation</div>
-  </nav>
+  </nav> -->
   <div id='landing page'>
     <div class='homebg d-flex justify-content-center align-items-center'>
       <div class="col-md-8 p-5 bg-light col-6">
@@ -17,15 +17,15 @@
               <p  class="card-text" >{{transcript}}</p>
               <div class="text-right">
                 <a href="#" class="btn btn-primary rounded-circle" :class="{'btn-danger':isRecord}"
-                @click.prevent="recordcontrol"><font-awesome-icon icon="microphone-alt"/></a></div>
+                @click.prevent="recordcontrol()"><font-awesome-icon icon="microphone-alt"/></a></div>
           </div>
         </div>
         <div v-if='showMic'>
           <div class='row justify-content-around d-flex' v-if="Micok">
-              <a class="col-5 p-4 d-flex bg-primary align-items-center justify-content-center text-white"
-              href='#' @click.prevent='startSurvey()'>Entet the Survey</a>
+              <a class="col-5 p-4 mt-3 d-flex bg-primary align-items-center justify-content-center text-white"
+              href='#' @click.prevent='startSurvey()'>Ready to Go ! Entet the Survey</a>
           </div>
-          <div v-else><p class="text-danger">We can't not detect your voice clearly</p></div>
+          <div v-else><p class="text-danger">We can't detect your voice clearly, please record again or check your device</p></div>
         </div>
       </div>
     </div>
@@ -43,17 +43,17 @@ export default {name: 'Welcome',
 data:function(){return {Micok:false,showMic:false,transcript:'',isRecord:false,confidence:0,}},
   methods:{
   startSurvey(){
-    const vm=this; vm.$router.push(`/${this.$firCase}`)}
-  },
+    const vm=this; vm.$router.push(`/${this.$firCase}`)},
   recordcontrol(){
     this.isRecord=!this.isRecord;
-    if (this.Record){
+    console.log(this.isRecord);
+    if (this.isRecord){
       this.showMic=false; 
       recognition.start();
       recognition.addEventListener('result', event => {
         const text = Array.from(event.results).map(result => result[0]).map(result => result.transcript).join('')
         let temp_con = Array.from(event.results).map(result=>result[0]).map(result=>result.confidence);
-        this.confidence=temp_con;
+        this.confidence=temp_con; 
         this.transcript = `${text}.`;
       });
     }
@@ -64,6 +64,8 @@ data:function(){return {Micok:false,showMic:false,transcript:'',isRecord:false,c
       if (acc>0.8){this.Micok=true} else{ this.Micok=false};
     }
   }
+  },
+  
 }
 </script>
 
