70ddc78ddd023bc7b3a42f831beb107480e45514
duckravel <al396421@uji.es>
2020-11-20 23:51:37 +0100

    reshape redirect page

--- src/components/pages/Redirect.vue
+++ src/components/pages/Redirect.vue
@@ -1,13 +1,15 @@
 <template>
 <div>   
   <div id='landing page'>
-        <div class='homebg d-flex justify-content-center align-items-center row' style="background-image:url('/data//patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg')">
-                <div class="col-lg-8 p-5 bg-light">
-                    <p class="h4 mb-5 align-middle text-center">
-                      Thank you for your time and contribution to our study.</p>   
-                    
+      <div class="backgroundshadow"></div>
+        <div class='homebg d-flex justify-content-center align-items-center row custombg'>
+                <div class="col-lg-5 p-5 shadow">
+                    <p class="h2 text-white align-middle text-center"><b>THANK YOU</b> </p>
+                    <p class="h5 text-white align-middle text-center"><b>for your contribution.</b></p> 
+                    <p class="mt-2 text-white align-middle text-center">We will proceed a quick user interview, please share with us your take away from the experiment.</p> 
                 </div>
-            </div>
+          </div>
+          
     </div>
   </div>
 </template>
@@ -18,5 +20,21 @@ export default {name: 'reditect'}
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ .custombg{
+  background-image: url(https://raw.githubusercontent.com/duckravel/Metadata/main/src/data/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+ }
+ .shadow{
+   background-color: rgba(0, 0, 0, 0.63);
+ }
+ .backgroundshadow{
+   background-color: rgba(0, 0, 0, 0.37);
+   position: absolute;
+    left: 0;
+    top:0;
+    height: 95vh;
+    width: 100vw;
+}
 
 </style>
